2974ea100c55a258935f07d2583f51ee9fb72fb6
jamesbrysmart <james.bryant18@gmail.com>
2018-04-02 23:53:33 +1200

    added slider component

---
+++ client/components/Slider.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Slider extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div className="slider">
+				
+      </div>
+    );
+  }
+}
